a35f932e8af5ec6a3cf445cc92da7087e55a0fb9
Nnamdi <nnamdi.b@gmail.com>
2022-05-26 18:35:12 +0200

    added some async function

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,25 @@
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
 
-export default function EventPage() {
+export default function EventPage({ slug }) {
   return (
     <Layout>
-      <h1>My Event</h1>
+      <h1>{slug}</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ query: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  console.log('slug :' + slug);
+
+  const events = await res.json();
+
+  console.log('events :' + events);
+
+  return {
+    props: {
+      evt: events[0],
+    },
+  };
 }
